589783db4d31ef7e50a8572da34db967a3e74bae
Rifafradita12 <rifafraditasafara@gmail.com>
2024-12-18 17:52:52 +0700

    Upload pertemuan11

--- data/students.js
+++ data/students.js
@@ -1,7 +1,4 @@
-const students = [
-    { id: 1, name: 'Sabrina Marliani', age: 21, major: 'IT' },
-    { id: 2, name: 'Rifa Fradita', age: 20, major: 'Business' },
-    { id: 3, name: 'Marsela', age: 20, major: 'Engineering' },
-];
+const student = ["Mikel", "Hannah", "Jonas"]; // student data
 
-module.exports = students;
+// export student data
+module.exports = student;
