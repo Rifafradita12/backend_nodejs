263c64ec24f1bf6f80fe70a54cec3e402eea516a
Rifafradita12 <rifafraditasafara@gmail.com>
2024-12-18 20:59:28 +0700

    Upload praktikum11

--- data/students.js
+++ data/students.js
@@ -1,7 +1,7 @@
 const students = [
-    { id: 1, name: 'Sabrina Marliani', age: 21, major: 'IT' },
-    { id: 2, name: 'Rifa Fradita', age: 20, major: 'Business' },
-    { id: 3, name: 'Marsela', age: 20, major: 'Engineering' },
+    { id: 1, name: 'kelvin sutirta', age: 19, major: 'Engineering' },
+    { id: 2, name: 'deden raffi', age: 19, major: 'Business' },
+    { id: 3, name: 'lutfi fuat', age: 19, major: 'TI' },
 ];
 
 module.exports = students;
